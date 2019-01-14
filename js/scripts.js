144048487153ce9eec7ef58fad34f0d91a0f9fdc
EspeIgira <espeigira@gmail.com>
2019-01-14 14:19:44 +0200

    Code finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
+
 function Player(playerName, score, turnScore) {
   this.playerName = playerName;
   this.score = score;
-  this.turn = turn;
+  this.turnScore = turnScore;
 }
-// roll:It is for one roll of a dice
-// turn: Amount of points added from many rolls of a dice
-// score:Final score
 
+// rollScore refers to one roll of the dice
+// turnScore refers to the total amount of points added from several rolls of the dice
+// score refers to total score
 
 Player.prototype.rollDie = function(){
-  var roll = 0
+  var rollScore = 0
   var diceArray = []
 
   var dice1 = Math.floor(Math.random()*(7-1)+1);
@@ -17,12 +18,12 @@ Player.prototype.rollDie = function(){
 
   if (dice1 !== 1 && dice2 !== 1){
     var totalDice = dice1 + dice2;
-    roll = totalDice;
-    this.turn += roll;
+    rollScore = totalDice;
+    this.turnScore += rollScore;
 
   }else{
-    roll = 0;
-    this.turn = 0;
+    rollScore = 0;
+    this.turnScore = 0;
     return "Hit One";
   }
 
@@ -30,15 +31,14 @@ Player.prototype.rollDie = function(){
   return diceArray;
 };
 
-
 Player.prototype.stop = function() {
-  this.score += this.turn
-  this.turn = 0;
+  this.score += this.turnScore
+  this.turnScore = 0;
 
 }
 
 Player.prototype.newTurn = function() {
-  this.turn = 0;
+  this.turnScore = 0;
 };
 
 Player.prototype.scoreCheck = function() {
@@ -48,7 +48,7 @@ Player.prototype.scoreCheck = function() {
 };
 
 Player.prototype.newGame = function(){
-  this.turn = 0;
+  this.turnScore = 0;
   this.score = 0;
 
 
@@ -56,15 +56,140 @@ Player.prototype.newGame = function(){
 
 
 
+$(document).ready(function() {
+
+  $(".first-row").hide();
+  $(".buttons-1").hide();
+  $(".buttons-2").hide();
+
+  $("#player-name-form").submit(function(event) {
+    event.preventDefault();
+
+    var player1input = $("#player-1").val();
+    var player2input = $("#player-2").val();
+
+    $(".player-1-name").text(player1input);
+    $(".player-2-name").text(player2input);
+
+    var player1 = new Player(player1input, 0, 0);
+    var player2 = new Player(player2input, 0, 0);
+
+    $(".first-row").fadeIn("fast");
+    $(".buttons-1").fadeIn("slow");
+
+// PLAYER ONE ROLL AND STOP BUTTON BELOW
+
+
+      var player1Rolls = function(){
+          $(".roll-1").click(function() {
+
+            var player1Dice = player1.rollDie();
+
+            $(".player-1-roll").text(" " + player1Dice);
+            $(".player-1-score").text(" " + player1.turnScore);
+            $(".player-1-total-score").text(" " + player1.score);
+
+            if(player1Dice === "Hit One"){
+              $(".buttons-1").fadeOut("slow");
+              $(".buttons-2").fadeIn("slow");
+            };
+
+            var winner = player1.scoreCheck();
+
+          });
+      };
+
+
+      var player1Stops = function(){
+        $(".stop-1").click(function(){
+
+          player1.stop();
+          $(".player-1-total-score").text(" " + player1.score);
+          $(".player-1-score").text(" " + player1.turnScore);
+
+
+          var winner = player1.scoreCheck();
+
+          if(winner === "Win"){
+            alert("Congratulations " + player1.playerName +" you win! GAME OVER!")
+            player1.newGame();
+            player2.newGame();
+            $(".player-1-total-score").text(" " + player1.score);
+            $(".player-1-score").text(" " + player1.turnScore);
+            $(".player-2-total-score").text(" " + player2.score);
+            $(".player-2-score").text(" " + player2.turnScore);
+            $(".player-1-roll").text(" ");
+            $(".player-2-roll").text(" ");
+
+          }else{
+            $(".buttons-1").fadeOut("slow");
+            $(".buttons-2").fadeIn("slow");
+          }
+
+        });
+      };
 
 
 
 
+// PLAYER TWO ROLL AND STOP BUTTON BELOW
+
+      var player2Rolls = function(){
+          $(".roll-2").click(function() {
+
+            var player2Dice = player2.rollDie();
+
+            $(".player-2-roll").text(" " + player2Dice);
+            $(".player-2-score").text(" " + player2.turnScore);
+            $(".player-2-total-score").text(" " + player2.score);
+
+            if(player2Dice === "Hit One"){
+              $(".buttons-2").fadeOut("slow");
+              $(".buttons-1").fadeIn("slow");
+            };
+
+            var winner = player2.scoreCheck();
+
+          });
+      };
 
 
 
+      var player2Stops = function(){
+        $(".stop-2").click(function(){
+
+          player2.stop();
+          $(".player-2-total-score").text(" " + player2.score);
+          $(".player-2-score").text(" " + player2.turnScore);
 
 
+          var winner = player2.scoreCheck();
 
+          if(winner === "Win"){
+            alert("Congratulations " + player2.playerName +" you win! GAME OVER!")
+            player1.newGame();
+            player2.newGame();
+            $(".player-1-total-score").text(" " + player1.score);
+            $(".player-1-score").text(" " + player1.turnScore);
+            $(".player-2-total-score").text(" " + player2.score);
+            $(".player-2-score").text(" " + player2.turnScore);
+            $(".player-1-roll").text(" ");
+            $(".player-2-roll").text(" ");
+            $(".buttons-2").fadeOut("slow");
+            $(".buttons-1").fadeIn("slow");
+          }else{
+            $(".buttons-2").fadeOut("slow");
+            $(".buttons-1").fadeIn("slow");
+          }
 
+        });
+      };
 
+      player1Rolls();
+      player1Stops();
+      player2Rolls();
+      player2Stops();
+
+  });
+
+});
