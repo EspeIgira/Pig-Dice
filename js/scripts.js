9b252fbc57ae8a2c3f6a45b8592e99ae18895c17
EspeIgira <espeigira@gmail.com>
2019-01-14 11:44:30 +0200

    prototype of rolldie

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 function Player(playerName, score, turnScore) {
   this.playerName = playerName;
   this.score = score;
-  this.turnScore = turnScore;
+  this.turn = turn;
 }
+// roll:It is for one roll of a dice
+// turn: Amount of points added from many rolls of a dice
+// score:Final score
+
+
+Player.prototype.rollDie = function(){
+  var roll = 0
+  var diceArray = []
+
+  var dice1 = Math.floor(Math.random()*(7-1)+1);
+  var dice2 = Math.floor(Math.random()*(7-1)+1);
+
+  if (dice1 !== 1 && dice2 !== 1){
+    var totalDice = dice1 + dice2;
+    roll = totalDice;
+    this.turn += roll;
+
+  }else{
+    roll = 0;
+    this.turn = 0;
+    return "Hit One";
+  }
+
+  diceArray.push(dice1, dice2);
+  return diceArray;
+};
+
